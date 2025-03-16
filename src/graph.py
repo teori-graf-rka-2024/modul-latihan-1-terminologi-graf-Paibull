025d345a9adbdef3ecc934749ee40ccc60d934b2
Paibull <90700967+Paibull@users.noreply.github.com>
2025-03-16 21:54:18 +0700

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -13,7 +13,7 @@ def dfs_traversal(G: nx.Graph, start: int) -> list[int]:
     return list(nx.dfs_preorder_nodes(G, start))
 
 def bfs_traversal(G: nx.Graph, start: int) -> list[int]:
-    return list(nx.bfs_edges(G, start))
+    return list(nx.bfs_tree(G, start))
 
 def find_shortest_path(G: nx.Graph, source: int, target: int) -> list[int]:
     return nx.shortest_path(G, source=source, target=target)
